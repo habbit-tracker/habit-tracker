c0181f3865d10322dc3a02600d88d527f0fabd5b
Briana Mason <briana.sauce@gmail.com>
2021-11-10 13:08:57 -0500

    added habit form which contains input fields for habit name and category and checkboxes for days of the week

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Button } from 'react-bootstrap';
+import { Button, Form } from 'react-bootstrap';
 import { useState, useRef } from 'react';
 
 function AddHabit() {
@@ -10,12 +10,91 @@ function AddHabit() {
   );
 }
 
+function HabitForm() {
+  return (
+    <Form className="form-container">
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Habit Name</Form.Label>
+        <Form.Control placeholder="Habit Name" />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Category</Form.Label>
+        <Form.Control placeholder="Category(optional)" />
+      </Form.Group>
+      <Form.Label>Target Days</Form.Label>
+      <Form.Group>
+        <Form.Check
+          inline
+          label="Monday"
+          name="group1"
+          type='checkbox'
+          defaultChecked='True'
+        //id=
+        />
+        <Form.Check
+          inline
+          label="Tuesday"
+          name="group1"
+          type='checkbox'
+          defaultChecked='True'
+        //id=
+        />
+        <Form.Check
+          inline
+          label="Wednesday"
+          name="group1"
+          type='checkbox'
+          defaultChecked='True'
+        //id=
+        />
+        <Form.Check
+          inline
+          label="Thursday"
+          name="group1"
+          type='checkbox'
+          defaultChecked='True'
+        //id=
+        />
+        <Form.Check
+          inline
+          label="Friday"
+          name="group1"
+          type='checkbox'
+          defaultChecked='True'
+        //id=
+        />
+        <Form.Check
+          inline
+          label="Saturday"
+          name="group1"
+          type='checkbox'
+          defaultChecked='True'
+        //id=
+        />
+        <Form.Check
+          inline
+          label="Sunday"
+          name="group1"
+          type='checkbox'
+          defaultChecked='True'
+        //id=
+        />
+      </Form.Group>
+      <Button variant="success" type="submit">
+        Create Habit
+      </Button>
+    </Form>
+  );
+
+}
+
 function App() {
 
   const args = JSON.parse(document.getElementById("data").text);
-
   return (
-    <AddHabit />
+    <>
+      <HabitForm />
+    </>
   );
 }
 
