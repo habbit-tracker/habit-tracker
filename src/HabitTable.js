c22357e5e10634398b3aca75a08c8b166f57920d
Briana Mason <briana.sauce@gmail.com>
2021-11-13 15:20:25 -0500

    For each habit, there are 7 squares for each day of the week.

--- src/HabitTable.js
+++ src/HabitTable.js
@@ -1,31 +1,50 @@
+import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Table } from 'react-bootstrap';
 
 function HabitLabel(props) {
     return (
-        <td>{props.habitTitle}</td>
+        <td className="habit-label">{props.habitTitle}</td>
+    );
+}
+
+function HabitSquare(props) {
+    return (
+        <td className="habit-square"></td>
     );
 }
 
 function HabitRow(props) {
+    const squares = [];
+    for (let i = 0; i < props.numOfDays; i++) {
+        squares.push(<HabitSquare />);
+    }
     return (
         <tr>
-            <th><HabitLabel habitTitle={props.habit['habit_title']} /></th>
+            <HabitLabel habitTitle={props.habit['habit_title']} />
+            {squares}
         </tr>
     );
 }
 
 export function HabitTable(props) {
     return (
-        <Table striped bordered hover>
+        <Table striped bordered hover className="weekly-view">
             <thead>
                 <tr>
                     <th>Habit</th>
+                    <th>M</th>
+                    <th>T</th>
+                    <th>W</th>
+                    <th>Th</th>
+                    <th>F</th>
+                    <th>Sa</th>
+                    <th>Su</th>
                 </tr>
             </thead>
             <tbody>
                 {props.habits.map((habit) => (
-                    <HabitRow habit={habit} />
+                    <HabitRow habit={habit} numOfDays={7} />
                 ))}
             </tbody>
         </Table>
